e8475ed637a472bf96bed05745a26f67d70fd72b
Andrew DeZutti <digpixs@gmail.com>
2016-03-04 13:41:49 -0500

    update

--- _header.php
+++ _header.php
@@ -5,12 +5,12 @@
     <link REL=”SHORTCUT ICON” HREF=”http://www.myberean.com/favicon.ico”>
     <link rel="stylesheet" href="css/main.css"/>
     <link rel="stylesheet" type="text/css" href="engine1/style.css" />
-	<link rel="stylesheet" href="css/audio.css"/>
+	  <link rel="stylesheet" href="css/audio.css"/>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" media="(max-width: 650px)" href="css/mobile.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-    <meta name="description" content="Berean exists for people of all generations and ethnicities to experience the incredible love of Christ.  It doesn't matter if you are new to church or further along in your spiritual journey.  We have a place for you here. ">
+    <meta name="description" content="Berean exists for people of all generations and ethnicities to experience the incredible love of Christ.">
     <meta name="keywords" content="08049, magnolia, church, easter, christmas, baptist">
 </head>
 <body>
@@ -24,11 +24,11 @@
                     <li class="nav-item"><a href="new.php" title="I'm new to the church">I'M NEW</a></li>
                     <li class="nav-item"><a href="about.php" title="All about Berean">ABOUT</a></li>
                     <li class="nav-item"><a href="prayer.php" title="How can we pray for you">PRAYER</a></li>
-                    <li class="nav-item"><a href="listen.php" title="Weekly messages and media">LISTEN</a></li> 
+                    <li class="nav-item"><a href="listen.php" title="Weekly messages and media">LISTEN</a></li>
                     <li class="nav-item"><a href="mini.php" title="Things going on in the Church">MINISTRIES</a></li>
                     <li class="nav-item"><a href="dir.php" title="How to get to Berean">DIRECTIONS</a></li>
                 </ul>
             </nav>
         </div>
-    </div>            
+    </div>
 </div>
