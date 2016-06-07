0026d5bad86284e4c2bd0385fa8d0afb6ef78f50
Andrew DeZutti <digpixs@gmail.com>
2016-06-07 08:56:06 -0400

    update

--- index.php
+++ index.php
@@ -18,32 +18,32 @@ body {
     <div id="wowslider-container1">
         <div class="ws_images">
             <ul>
-                <!-- <li><img src="img/info_cmaseve.jpg" alt="" title="" id="wows1_cmas"/></li> -->
-                <!-- <li><img src="img/info_cmas.jpg" alt="" title="" id="wows1_cmas"/></li> -->
+                <!-- <li><img src="img/info_cmaseve.jpg" alt="" title="" id="wows1_hd"/></li> -->
+                <!-- <li><img src="img/info_cmas.jpg" alt="" title="" id="wows1_hd"/></li> -->
                 <!-- <li><img src="img/info_serv_can.jpg" alt="" title="" id="wows1_sc"/></li> -->
                 <!-- <li><img src="img/ban_awana_can.jpg" alt="" title="" id="wows1_ac"/></li> -->
-                <!-- <li><img src="img/ban_bsm_can.jpg" alt="" title="" id="wows1_bsc"/></li> -->
-                <!-- <li><img src="img/info_thanksgiving_pp.jpg" alt="Thanksgiving Prayer" title="" id="wows1_tg"/></li> -->
-                <!-- <li><img src="img/info_thanksgiving.jpg" alt="Thanksgiving Prayer" title="" id="wows1_tg"/></li> -->
-                <!-- <li><img src="img/info_new_year.jpg" alt="" title="" id="wows1_ny"/></li> -->
-								<li><img src="img/bbc_info_easter.jpg" alt="Easter Service" title="" id="wows1_ny"/></li>
-                <li><img src="img/ban_romans.jpg" alt="Reshaping Romans" title="" id="wows1_nm"/></a></li>
-                <li><img src="img/info_awana.jpg" alt="The start of awana" title="" id="wows1_aw"/></li>
-                <li><a href="mini.php"><img src="img/ban_life_info.jpg" alt="Life Groups" title="" id="wows1_lg"/></a></li>
+                <!-- <li><img src="img/info_thanksgiving_pp.jpg" alt="Thanksgiving Prayer" title="" id="wows1_hd"/></li> -->
+                <!-- <li><img src="img/info_thanksgiving.jpg" alt="Thanksgiving Prayer" title="" id="wows1_hd"/></li> -->
+                <!-- <li><img src="img/info_new_year.jpg" alt="" title="" id="wows1_hd"/></li> -->
+								<!-- <li><img src="img/bbc_info_easter.jpg" alt="He is Risen" title="" id="wows1_hd"/></li> -->
+								<!-- <li><img src="img/ban_memday.jpg" alt="memorial day" title="" id="wows1_shd"/></li> -->
+								<!-- <li><a href="mini.php"><img src="img/ban_connection.jpg" alt="connecting with God" title="" id="wows1_event"/></a></li> -->
+								<li><a href="mini.php"><img src="img/ban_summer.jpg" alt="summer groups" title="" id="wows1_event2"/></a></li>
+                <li><a href="listen.php"><img src="img/ban_romans.jpg" alt="Reshaping Romans" title="" id="wows1_nm"/></a></li>
+                <!-- <li><img src="img/info_awana.jpg" alt="The start of awana" title="" id="wows1_aw"/></li> -->
+                <!-- <li><a href="mini.php"><img src="img/ban_life_info.jpg" alt="Life Groups" title="" id="wows1_lg"/></a></li> -->
                 <li><a href="new.php"><img src="img/ban_new.jpg" alt="I'm New to the church" title="" id="wows1_new"/></a></li>
                 <li><a href="https://www.facebook.com/BereanSM?ref=hl" target="_blank"><img src="img/info_bsm.jpg" alt="Berean Student Ministry" title="" id="wows1_bsm"/></a></li>
+								<!-- <li><img src="img/ban_bsm_can.jpg" alt="" title="" id="wows1_bsm"/></li> -->
             </ul>
         </div>
         <div class="ws_bullets">
-            <!-- <a href="#" title="">cmas</a> -->
-            <!-- <a href="#" title="">sc</a> -->
-            <a href="#" title="">nm</a>
-            <a href="#" title="">ac</a>
-            <a href="#" title="">bsc</a> -->
-            <!-- <a href="#" title="">tg</a> -->
-            <!-- <a href="#" title="">ny</a> -->
-            <!-- <a href="#" title="">aw</a> -->
-            <a href="#" title="">lg</a>
+						<!-- <a href="#" title="">hd</a> -->
+						<!-- <a href="#" title="">shd</a> -->
+						<!-- <a href="#" title="">event</a> -->
+						<a href="#" title="">event2</a>
+						<a href="#" title="">nm</a>
+            <!-- <a href="#" title="">lg</a> -->
             <a href="#" title="">new</a>
             <a href="#" title="">bsm</a>
        	</div>
@@ -53,17 +53,19 @@ body {
 
 
     <div id="serv_time">
-        <ul>
-            <li id="serv_item"><span>Sunday 10:30am</span> - Worship Gathering</li>
-            <li id="serv_item"><span>Wednesday 7:00pm</span> - <a href="mini.php">Life Groups, Awana, Youth Group</a></li>
-        </ul>
+			<div id="serv_item"><span>Sunday 10:30am</span> - Worship Gathering</div>
+			<!-- <div id="serv_item"><span>Wednesday 7:00pm</span> - <a href="mini.php">Life Groups, Awana, Youth Group</a></div> -->
+			<div id="serv_item"><span>Tuesday 7:00pm</span> - Ladies - Listening to God</div>
+			<div id="serv_item"><span>Wednesday 7:00pm</span> - Mixed Group - The Truth Project</div>
+			<div id="serv_item"><span>Sunday bi-weekly 4:00pm</span> - Couples (Healthy Marriage)</div>
     </div>
 
 
     <div id="hp_info_con">
-        <div id="hp_info_sig"><a href="https://www.summitu.edu/LYFE" title="Our History" target="_blank"><img src="img/info_ll2016.jpg" alt="Ladies Lyfe 2016"></a></div>
+				<div id="hp_info_sig"><a href="mini.php" title="Grow with us"><img src="img/info_connection.jpg" alt="connection station"></a></div>
+        <div id="hp_info_sig"><a href="mini.php" title="summer groups" target="_blank"><img src="img/info_summer.jpg" alt="Summer Groups"></a></div>
         <div id="hp_info_sig"><a href="prayer.php" title="How Can We Pray For You"><img src="img/ele_pray.jpg" alt="How Can We Pray For You"></a></div>
-        <div id="hp_info_sig"><a href="mini.php" title="Grow with us"><img src="img/info_life.jpg" alt="Life Groups"></a></div>
+        <!-- <div id="hp_info_sig"><a href="mini.php" title="Grow with us"><img src="img/info_life.jpg" alt="Life Groups"></a></div> -->
         <div id="hp_info_sig"><a href="new.php" title="I'm New"><img src="img/ele_new.jpg" alt="I'm New"></a></div>
 
     </div>
--- mini.php
+++ mini.php
@@ -14,27 +14,39 @@ body {
 
 <div class="main_container">
 
-<div class="info_area">
-    <div id=""><img src="img/ban_life2.jpg" alt="Growth Groups"></div>
+	<div class="info_area">
+    <!-- <div id=""><img src="img/ban_connection_lg.jpg" alt="connection stations"></div>
     <div id="info_block">
-        <div id="info_sig"><p><span>Wednesday Nights @ 7:00pm</span> Life Group is designed for adults to learn more about the Bible in a group environment.  It enables discussion and relationship building with other adults. The class will be meeting in the living room (down the back hallway, last room on the right upstairs). </p><br></div>
+        <div id="info_sig"><p><span>Wednesday Night @ 7:00pm</span> Connecting with God is challenging because of so many distractions.    The Connection station will have several stations throughout the church that will guide you to connect with God in a creative and private way.  Whether you’re a beginner or a veteran, this is an opportunity to spend   30 min to an hour moving from station to station connecting with God.</p><br></div>
 
-    </div> 
+    </div> -->
 
-    <div id=""><img src="img/ban_youth.jpg" alt="youth ministries banner"></div>
-     
-    <div id="info_quad"><h1>Nursery</h1><p>During our church services we have a nursery ministry for children from 6 weeks till 3 years. You'll find our nursery to be clean, organized, safe and fun for them. Our staff consists of volunteer church members that love children &amp; don't mind changing that stinky diaper!</p>
+    <div id=""><img src="img/ban_summer_mini.jpg" alt="summer time groups"></div>
+
+		<div id="info_block">
+        <div id="info_sig">
+					<ul>
+						<li><span>Ladies</span> (Listening to God) starting Wednesday, June 8th @ 7pm.</li>
+						<li><span>Mixed</span> (Truth Project) starting Tuesday, June 7th @ 7pm.</li>
+						<li><span>Couples</span> (Healthy Marriage) bi-weekly starting Sun, June 12th @ 4pm.</li>
+					</ul>
+				</div>
+
     </div>
-    
+
+
+    <!-- <div id="info_quad"><h1>Nursery</h1><p>During our church services we have a nursery ministry for children from 6 weeks till 3 years. You'll find our nursery to be clean, organized, safe and fun for them. Our staff consists of volunteer church members that love children &amp; don't mind changing that stinky diaper!</p>
+    </div>
+
     <div id="info_quad"><h1>Kid's Church</h1><p>Kid's Church is for kid's from 4 years to 6th grade. Families get to come to the Worship Gathering together, but after the song service the kids go to Kid's Church where they learn about the Bible and meet new friends.</p>
     </div>
-    
+
     <div id="info_quad"><h1>Awana</h1><p>Awana Clubs is for children ages 3 to 6th grade. Awana is a safe and fun environment where kids learn about the Bible, meet new friends, and have a lot of fun. Children are welcome to try us out for one night, or come weekly for the entire school year year (September - May). Wednesday nights from 7-8:30pm</p>
     </div>
-    
+
     <div id="info_quad"><h1>Teens</h1><p>Berean Student Ministry is an extension of Berean in Southern New Jersey area. We are committed to coming alongside students and giving them a place where they can feel accepted and cared for. Our mission is to connect students with God and others.</p>
-    <p><a href="https://www.facebook.com/BereanSM?ref=hl" target="_blank">Click HERE for BSM</a></p>
+    <p><a href="https://www.facebook.com/BereanSM?ref=hl" target="_blank">Click HERE for BSM</a></p> -->
     </div>
 </div>
-    
-<?php include '_footer.php'; ?> 
+
+<?php include '_footer.php'; ?>
